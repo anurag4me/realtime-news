0b246859ffcabb671daabd221b1bb5dc4f2f25cf
anurag4me <anuragmishra5033@gmail.com>
2025-05-10 19:39:20 +0530

    Fixed blocked CORS

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN?.replace(/\/$/, ''),
     methods: ["GET", "POST"],
     credentials: true
   }
@@ -21,7 +21,7 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: process.env.CORS_ORIGIN,
+  origin: process.env.CORS_ORIGIN?.replace(/\/$/, ''),
   credentials: true
 }));
 app.use(express.json());
